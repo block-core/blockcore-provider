e03ae40f87388551d9849d11d999d50120590347
SondreB <sondre@outlook.com>
2021-09-13 21:31:37 +0200

    chore: auto-format source and add extension suggestion

--- src/Provider.ts
+++ src/Provider.ts
@@ -3,26 +3,26 @@ import axios, { AxiosError } from "axios";
 export class Provider {
   private baseUrl: string;
   private response: {
-      data: unknown | undefined
-      error?: AxiosError; 
-    }
+    data: unknown | undefined
+    error?: AxiosError;
+  }
 
   public constructor(baseUrl?: string) {
     this.baseUrl = baseUrl || 'http://city.indexer.blockcore.net';
-    this.response = {data: undefined}
+    this.response = { data: undefined }
   }
   protected async executeGet<AxiosResponse>(endpoint: string): Promise<AxiosResponse | undefined> {
     await axios.get(endpoint)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
+      .then((res) => {
+        this.response = {
+          data: res.data,
+          error: undefined
+        }
+      })
+      .catch((error) => {
+        this.response.error = error
+      })
+
     return this.response as unknown as AxiosResponse;
   }
 
@@ -33,7 +33,7 @@ export class Provider {
   public async getCirculatingSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
     return this.executeGet(this.baseUrl + "/api/insight/supply/circulating");
   }
-  
+
   public async getTotalSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
     return this.executeGet(this.baseUrl + "/api/insight/supply/total");
   }
@@ -59,11 +59,11 @@ export class Provider {
 
   public async getAddressUnconfirmedTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions/unconfirmed`);
- }
+  }
 
   public async getAddressSpentTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions/spent`);
-  } 
+  }
 
   public async getAddressUnspentTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions/unspent`);
@@ -71,37 +71,37 @@ export class Provider {
 
   public async getMempoolTransactions<AxiosResponse>(): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/mempool/transactions`);
- }
+  }
 
   public async getMempoolTransactionsCount<AxiosResponse>(): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/mempool/transactions/count`);
- }
+  }
 
   public async getTransactionById<AxiosResponse>(id: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/transaction/${id}`);
- }
+  }
 
   public async getBlock<AxiosResponse>(): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/block`);
- }
+  }
 
   public async getBlockTransactionsByHash<AxiosResponse>(hash: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/block/${hash}/transactions`);
- }
- 
+  }
+
   public async getBlockByHash<AxiosResponse>(hash: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/block/${hash}`);
- }
+  }
 
   public async getBlockByIndex<AxiosResponse>(index: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/block/index/${index}`);
- }
- 
+  }
+
   public async getBlockTransactionsByIndex<AxiosResponse>(index: string): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/block/index/${index}/transactions`);
- }
+  }
 
-   public async getLatestBlock<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+  public async getLatestBlock<AxiosResponse>(): Promise<AxiosResponse | undefined> {
     return this.executeGet(`${this.baseUrl}/api/query/block/latest`);
- }
+  }
 }
