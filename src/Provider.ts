d991a868601832e119a1d2e7f144e7dc24d1814b
GONZdev <grgsalamanca@gmail.com>
2021-09-13 06:56:52 +0100

    chore: shortened provider.ts and added some more tests

--- src/Provider.ts
+++ src/Provider.ts
@@ -11,9 +11,8 @@ export class Provider {
     this.baseUrl = baseUrl || 'http://city.indexer.blockcore.net';
     this.response = {data: undefined}
   }
-
-  public async getSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-    await axios.get(this.baseUrl + "/api/insight/supply")
+  protected async executeGet<AxiosResponse>(endpoint: string): Promise<AxiosResponse | undefined> {
+    await axios.get(endpoint)
     .then((res) => {
       this.response = {
         data: res.data,
@@ -25,315 +24,84 @@ export class Provider {
      })
      
     return this.response as unknown as AxiosResponse;
- }
+  }
 
-   public async getCirculatingSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+  public getSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+    return this.executeGet(this.baseUrl + "/api/insight/supply")
+  }
 
-    await axios.get(this.baseUrl + "/api/insight/supply/circulating")
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
-    public async getTotalSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(this.baseUrl + "/api/insight/supply/total")
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
+  public async getCirculatingSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+    return this.executeGet(this.baseUrl + "/api/insight/supply/circulating");
+  }
+  
+  public async getTotalSupply<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+    return this.executeGet(this.baseUrl + "/api/insight/supply/total");
+  }
 
   public async getEstimateRewards<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+    return this.executeGet(this.baseUrl + "/api/insight/rewards");
+  }
+  public async getWallets<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+    return this.executeGet(this.baseUrl + "/api/insight/wallets");
+  }
 
-    await axios.get(this.baseUrl + "/api/insight/rewards")
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
- public async getWallets<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(this.baseUrl + "/api/insight/wallets")
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
-
- public async getRichList<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(this.baseUrl + "/api/insight/richlist")
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
+  public async getRichList<AxiosResponse>(): Promise<AxiosResponse | undefined> {
+    return this.executeGet(this.baseUrl + "/api/insight/richlist");
+  }
 
   public async getAddress<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
+    return this.executeGet(`${this.baseUrl}/api/query/address/${address}`);
+  }
 
-    await axios.get(`${this.baseUrl}/api/query/address/${address}`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+  public async getAddressTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
+    return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions`);
+  }
+
+  public async getAddressUnconfirmedTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
+    return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions/unconfirmed`);
  }
 
-   public async getAddressTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/address/${address}/transactions`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
-
-   public async getAddressUnconfirmedTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/address/${address}/transactions/unconfirmed`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
-
-   public async getAddressSpentTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/address/${address}/transactions/spent`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
+  public async getAddressSpentTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
+    return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions/spent`);
+  } 
 
   public async getAddressUnspentTransactions<AxiosResponse>(address: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/address/${address}/transactions/unspent`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
- }
+    return this.executeGet(`${this.baseUrl}/api/query/address/${address}/transactions/unspent`);
+  }
 
   public async getMempoolTransactions<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/mempool/transactions`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/mempool/transactions`);
  }
 
   public async getMempoolTransactionsCount<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/mempool/transactions/count`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/mempool/transactions/count`);
  }
 
   public async getTransactionById<AxiosResponse>(id: string): Promise<AxiosResponse | undefined> {
-    // type TResponse = {
-    //   data: unknown | undefined
-    //   error?: AxiosError; 
-    // }
-    // const response: TResponse  = { data: undefined }
-    await axios.get(`${this.baseUrl}/api/query/transaction/${id}`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/transaction/${id}`);
  }
 
   public async getBlock<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/block`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/block`);
  }
 
   public async getBlockTransactionsByHash<AxiosResponse>(hash: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/block/${hash}/transactions`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/block/${hash}/transactions`);
  }
  
   public async getBlockByHash<AxiosResponse>(hash: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/block/${hash}`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/block/${hash}`);
  }
 
-   public async getBlockByIndex<AxiosResponse>(index: string): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/block/index/${index}`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+  public async getBlockByIndex<AxiosResponse>(index: string): Promise<AxiosResponse | undefined> {
+    return this.executeGet(`${this.baseUrl}/api/query/block/index/${index}`);
  }
  
   public async getBlockTransactionsByIndex<AxiosResponse>(index: string): Promise<AxiosResponse | undefined> {
-    // type TResponse = {
-    //   data: unknown | undefined
-    //   error?: AxiosError; 
-    // }
-    // const response: TResponse  = { data: undefined }
-    await axios.get(`${this.baseUrl}/api/query/block/index/${index}/transactions`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/block/index/${index}/transactions`);
  }
 
    public async getLatestBlock<AxiosResponse>(): Promise<AxiosResponse | undefined> {
-
-    await axios.get(`${this.baseUrl}/api/query/block/latest`)
-    .then((res) => {
-      this.response = {
-        data: res.data,
-        error: undefined
-      }
-    })
-    .catch((error) => {
-      this.response.error = error
-     })
-     
-    return this.response as unknown as AxiosResponse;
+    return this.executeGet(`${this.baseUrl}/api/query/block/latest`);
  }
 }
--- test/Provider.test.ts
+++ test/Provider.test.ts
@@ -3,7 +3,7 @@ import { Provider } from '../src/Provider'
 
 describe("Provider test suit", () => {
   let provider: Provider;
-  const getSupplyMock = jest.fn();
+  const getSupplyMock: any = jest.fn();
   const someResponse = {
     data: {},
     error: {} || undefined
@@ -15,8 +15,151 @@ describe("Provider test suit", () => {
   
   afterEach(() => jest.clearAllMocks());
   
-  it('should receive and object with data and error', async () => {
-    const result = await provider.getSupply();
-    expect(result).toMatchObject(someResponse)
+  // it('should match data and error object for each API call with no parameters', async () => {
+  // [
+  //   provider.getSupply(),
+  //   provider.getCirculatingSupply(), 
+  //   provider.getTotalSupply(),
+  //   provider.getWallets(),
+  //   provider.getRichList(),
+  //   provider.getMempoolTransactions(),
+  //   provider.getMempoolTransactionsCount(),
+  //   provider.getBlock(),
+  //   provider.getLatestBlock()
+  // ]
+  // .forEach(async call => {    
+  //   return expect(call).resolves.toMatchObject(someResponse)
+  // })
+  
+  // })
+  // it('should return a data and error object when calling getAddress Provider method', async () => {
+  //   return expect(provider.getAddress(getSupplyMock)).resolves.toMatchObject(someResponse);
+  // })
+
+  it('should test getSupply method', async () => {
+    const result: any = await provider.getSupply();
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data.total > 303049697).toBeTruthy();
+    expect(result.data.rewards > 2158270).toBeTruthy();
+    expect(result.data.height > 1218270).toBeTruthy();
   })
+  it('should test if getCirculatingSupply method returns a number', async () => {
+    const result: any = await provider.getCirculatingSupply();
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).not.toBeNaN()
+  })
+  it('should test if getTotalSupply method returns a number', async () => {
+    const result: any = await provider.getTotalSupply();
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).not.toBeNaN()
+  })
+  it('should test if getEstimateRewards method returns a number', async () => {
+    const result: any = await provider.getEstimateRewards();
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).not.toBeNaN()
+  })
+  it('should test if getWallets method returns an object containing Burnt account', async () => {
+    const result: any = await provider.getWallets();
+    
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+      expect.objectContaining({ 
+        "name": "Burnt",
+        "type": "Burn",
+        "initialAmount": 0.0,
+      })
+    ])
+    )
+  })
+  it('should call getRichList and contain a handpicked random account', async () => {
+    const someAddress = {
+        "address": "CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV",
+    }
+    const result: any = await provider.getRichList();
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(expect.arrayContaining([expect.objectContaining(someAddress)]))
+  })
+  it('should test if getAddress method returns the address passed as argument', async () => {
+
+    const result: any = await provider.getAddress("CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV");
+    const someAddress = {
+      "address": "CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV",
+    }
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(expect.objectContaining(someAddress))
+  })
+  it('should test if getAddressTransactions method returns the address transactions passed as argument', async () => {
+
+    const result: any = await provider.getAddressTransactions("CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV");
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+      expect.objectContaining({
+      "symbol": "CITY",
+      "blockHash": "ecb1aca33af0bb4885b3fc898efbabe5cbcf58e77d7d20bde5cc51ced75bbbb9",
+      "blockIndex": 1339023,
+      "timestamp": 1631468016,
+      "transactionId": "6ea66e4ae9c3f19b9e410de16f71ed6ec4b0e7111d8ed3083f0b6c5c43093510"
+    })
+    ])
+    )
+  })
+  it('should test if getAddressUnconfirmedTransactions method returns the address unconfirmed transactions passed as argument', async () => {
+
+    const result: any = await provider.getAddressUnconfirmedTransactions("CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV");
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          "symbol": "CITY",
+          "blockHash": "ecb1aca33af0bb4885b3fc898efbabe5cbcf58e77d7d20bde5cc51ced75bbbb9",
+          "blockIndex": 1339023,
+          "timestamp": 1631468016,
+          "transactionId": "6ea66e4ae9c3f19b9e410de16f71ed6ec4b0e7111d8ed3083f0b6c5c43093510"
+        })
+      ])
+    )  
+  })
+  it('should test if getAddressSpentTransactions method returns the address spent transactions passed as argument', async () => {
+
+    const result: any = await provider.getAddressSpentTransactions("CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV");
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          "symbol": "CITY",
+          "blockHash": "f1cda3861b642fa6ee188655f53c8f209c0a2033f5105aa350e73f92da3b74fe",
+          "blockIndex": 1333716,
+          "timestamp": 1631112128,
+          "transactionId": "8de7841ac0944e00fcc03436b4d08c17218bcb21f71b3bcb2a0f99ba3185ff6f",        })
+      ])
+    )  
+  })
+  it('should test if getAddressUnspentTransactions method returns the address unspent transactions passed as argument', async () => {
+
+    const result: any = await provider.getAddressUnspentTransactions("CbFj5PM6oxUBBUM3LBejPFx4LZSnLJK6ZV");
+
+    expect(result).toMatchObject(someResponse);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          "symbol": "CITY",
+          "blockHash": "ecb1aca33af0bb4885b3fc898efbabe5cbcf58e77d7d20bde5cc51ced75bbbb9",
+          "blockIndex": 1339023,
+          "timestamp": 1631468016,
+          "transactionId": "6ea66e4ae9c3f19b9e410de16f71ed6ec4b0e7111d8ed3083f0b6c5c43093510"        })
+      ])
+    )  
+  })
+
 });
