f46d75d4762baccfdc47f5c7a6db5c719ed510b0
SondreB <sondre@outlook.com>
2022-11-09 23:19:59 +0100

    Extend the IdentityProvider with methods from the TBD RFC

--- src/IdentityProvider.ts
+++ src/IdentityProvider.ts
@@ -1,9 +1,18 @@
 export class IdentityProvider {
-    // https://github.com/TBD54566975/janky-wallet/blob/main/rfc/web5-did-supported-methods.md
-    /** This method can be used by clients to become aware of the DID methods supported by a wallet. */
+	// https://github.com/TBD54566975/janky-wallet/blob/main/rfc/web5-did-supported-methods.md
+	/** This method can be used by clients to become aware of the DID methods supported by a wallet. */
 	async supportedMethods(): Promise<string[]> {
 		return ['did:is', 'did:jwk', 'did:key'];
 	}
 
+	// https://github.com/TBD54566975/janky-wallet/blob/main/rfc/web5-did-authn.md
+	/** Initiates DID-based passwordless registration / login flows */
+	async authn() {
+		throw Error('Not implemented.');
+	}
 
+	// https://github.com/TBD54566975/janky-wallet/blob/main/rfc/web5-did-request.md
+	async request() {
+		throw Error('Not implemented.');
+	}
 }
